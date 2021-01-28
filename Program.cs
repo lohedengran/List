61e3065c330c2a1e34e47b9dbb4ab9b14b8937b2
Lo Hedengran <lohedengran@gmail.com>
2021-01-28 14:02:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Adam", "Eva" };
+            PrintList(names);
+
+            AddName(names);
+
+            RemoveName(names);
+
+            SortNames(names);
+
+        }
+
+
+        private static void PrintList(List<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void AddName(List<string> names)
+        {
+            bool addMoreNames = true;
+            do
+            {
+            Console.WriteLine("Would you like to add a name to the list? Press Y.");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+                if (answer == "y")
+                {
+                    Console.WriteLine("Add a name to list: ");
+                    names.Add(Console.ReadLine());
+                }
+                else
+                    addMoreNames = false;
+
+            } while (addMoreNames);   
+            
+        }
+        private static void RemoveName(List<string> names)
+        {
+            Console.Clear();
+            PrintList(names);
+            Console.WriteLine("What name would you like to remove?");
+            string input = Console.ReadLine();
+            if (names.Contains(input))
+                names.Remove(input);
+            else
+                Console.WriteLine("Name not found");
+            PrintList(names);
+        }
+        private static void SortNames(List<string> names)
+        {
+            Console.Clear();
+            names.Sort();
+            Console.WriteLine("The names in alphabetical order:");
+            PrintList(names);
+        }
+
+    }
+}
